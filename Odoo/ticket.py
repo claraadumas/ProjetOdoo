6e05b6878c7301ea81a6dc92ef2002738a2b30d8
ArmandMichaud <117903627+ArmandMichaud@users.noreply.github.com>
2023-05-04 09:45:12 +0200

    Add files via upload

--- Odoo/ticket.py
+++ Odoo/ticket.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
 
-import xmlrpc.client
+import xmlrpc.client  ##bibliothèque permettant de faire des requètes api pour Odoo, voir plus d'information sur le site : https://www.odoo.com/documentation/16.0/developer/reference/external_api.html
 import sys
 import re
 
-url = 'http://localhost:8069'
-db = 'ProjetOdoo'
-username = 'armand.michaud@hotmail.com'
-password = 'arbre1001'
+
+###identifiant de connexion Odoo //A changer 
+
+
+url = 'http://localhost:8069'  ##adresse du serveur, peut être local ou pas
+db = 'ProjetOdoo' ##nom de la base de données
+username = 'armand.michaud@hotmail.com' ##adresse mail de connexion
+password = 'arbre1001'  ##mot de passe de connexion
 
 
 def removeendline(value):
+    ##permet d'enlever les fins de lignes d'un string
     return ''.join(value.splitlines())
 
 
@@ -19,6 +24,7 @@ def removeendline(value):
 
 
 def recuperation_ticket():
+    ##permet la récupération des tickets. Enlève les fin de ligne s'il y en a
     res=models.execute_kw(db, uid, password, 'helpdesk.ticket', 'search_read', [], {'fields': ['number', 'name', 'description','partner_name','partner_email'] })
 
     for ticket in res :
@@ -42,8 +48,9 @@ def recuperation_ticket():
         
         
 
-      
+
 def creation_ticket(nom_ticket,description_ticket,nom_partner,email_partner):
+    ##permet la création d'un ticket, prend en paramètre le nom du ticket, sa descrption, le nom du créateur du ticket et son email
     nom_partner=nom_partner.replace('_', ' ')
     ticket_data = {
         'name': nom_ticket,
@@ -56,18 +63,21 @@ def creation_ticket(nom_ticket,description_ticket,nom_partner,email_partner):
     
 
 try:
+    ##Connexion à l'api
     common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
     uid = common.authenticate(db, username, password, {})
     # Create a new customer record
     models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
     
-    fichier = open("Odoo/commentaire.txt", "r")
-    description = fichier.read()
-    fichier.close()
     
-
+    
+    ##permet en fonction des arguments passés lors de l'execution du script python d'obtenir le type de requete a effectuer. La récupération des informations se fait dans le print
     if len( sys.argv ) > 1:
-        if sys.argv[1]=='create_ticket' :   
+        if sys.argv[1]=='create_ticket' :  
+            ##permet d'obtenir le commentaire de l'utilisateur
+            fichier = open("Odoo/commentaire.txt", "r")
+            description = fichier.read()
+            fichier.close() 
             creation_ticket(sys.argv[2],description,sys.argv[3],sys.argv[4])
         elif sys.argv[1]=='recup_ticket':
             recuperation_ticket()
